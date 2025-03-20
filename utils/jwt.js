84544dccd46beb435844662f34ee4d4f104e267a
AVVKavvk <kumawatvipin066@gmail.com>
2025-03-20 18:43:28 +0530

    socket.io cookie issue, not setting

--- utils/jwt.js
+++ utils/jwt.js
@@ -7,7 +7,9 @@ const generateToken = (userId, res)=>{
 
   res.cookie("jwt_token",token,{
     maxAge:7*24*60*60*1000,
-    httpOnly:true
+    httpOnly:true,
+    sameSite:"None",
+    secure:true
   })
 }
 
